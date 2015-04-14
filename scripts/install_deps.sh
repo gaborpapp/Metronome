78b1fb07895a63b88c58e317a69799dbc553c529
Gabor Papp <gabor.papp@gmail.com>
2015-04-14 17:00:15 +0200

    updated dependency install script with OpenCV

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -3,3 +3,6 @@
 # Cinder-NI-libs from bitbucket.org
 curl -L -C - -O https://bitbucket.org/gaborpapp/cinder-libs/downloads/Cinder-NI-lib-2.3.0.4.tar.gz
 tar zxvf Cinder-NI-lib-2.3.0.4.tar.gz -C blocks/Cinder-NI
+
+curl -L -C - -O https://bitbucket.org/gaborpapp/cinder-libs/downloads/Cinder-OpenCV-lib-2.4.9.zip
+unzip Cinder-OpenCV-lib-2.4.9.zip -d blocks/
